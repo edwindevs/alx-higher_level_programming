7fc43c5af4b3bd44d203823ed8ae137acceaa5a3
edwindevs <edmbogo@outlook.com>
2022-06-29 00:03:12 +0000

    Task 14. Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i, c in enumerate(range(122, 96, -1)):
-    print("{:c}".format(c - 32 if i % 2 else c), end="")    
+for char in reversed(range(97, 123)):
+    print("{:c}".format(char if (char % 2 == 0) else (char - 32)), end='')
