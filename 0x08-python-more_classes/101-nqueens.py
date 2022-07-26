d815fd6875b11d9ba5ef82a2d49b84862eaf286a
edwindevs <edmbogo@outlook.com>
2022-07-26 19:29:46 +0000

    RSA-Factoring-Challenge

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,57 +1,89 @@
 #!/usr/bin/python3
-"""Solution to the N queens problem with backtracking"""
+'''A module for solving the N queens problem.
+'''
 import sys
 
 
-class Solution:
-    """defines the solution to the N queens problem using backtracking
-    algorithm"""
-
-    def solveNQueens(self, n: int) -> list[list[str]]:
-        col = set()
-        posDiag = set() # (r + c)
-        negDiag = set() # (r - c)
-
-        res = []
-        board = [["."] * n for i in range(n)]
-        
-        def backtrack(r):
-            if r == n:
-                copy = ["".join(row) for row in board]
-                res.append(copy)
-                return
-
-            for c in range(n):
-                if c in col or (r + c) in posDiag or (r - c) in negDiag:
-                    continue
-
-                col.add(c)
-                posDiag.add(r + c)
-                negDiag.add(r - c)
-                board[r][c] = 'Q'
-
-                backtrack(r + 1)
-
-                col.remove(c)
-                posDiag.remove(r + c)
-                negDiag.remove(r - c)
-                board[r][c] = '.'
-
-        backtrack(0)
-        return res
+result = []
+n = 0
+pos = None
 
 
-if __name__ == "__main__":
+def get_input():
+    '''Retrieves and validates this program's argument.
+    Returns:
+        int: The size of the chessboard.
+    '''
+    global n
+    n = 0
     if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+        print('Usage: nqueens N')
         sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except Exception:
+        print('N must be a number')
+        sys.exit(1)
+    if n < 4:
+        print('N must be at least 4')
+        sys.exit(1)
+    return n
 
-    if not sys.argv[1].isnumeric():
-        print("N must be a number")
-        sys.exit(1)
 
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    for l in Solution().solveNQueens(int(sys.argv[1])):
-        print(l)
+def is_attacking(pos0, pos1):
+    '''Checks if the positions of two queens are in an attacking mode.
+    '''
+    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
+        return True
+    return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
+
+
+def group_exists(group):
+    '''Checks if a group exists in the list of solutions.
+    '''
+    global result
+    for stn in result:
+        i = 0
+        for stn_pos in stn:
+            for grp_pos in group:
+                if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
+                    i += 1
+        if i == n:
+            return True
+    return False
+
+
+def build_solution(row, group):
+    '''Builds a solution for the n queens problem.
+    '''
+    global result
+    global n
+    if row == n:
+        tmp0 = group.copy()
+        if not group_exists(tmp0):
+            result.append(tmp0)
+    else:
+        for col in range(n):
+            a = (row * n) + col
+            matches = zip(list([pos[a]]) * len(group), group)
+            used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
+            group.append(pos[a].copy())
+            if not any(used_positions):
+                build_solution(row + 1, group)
+            group.pop(len(group) - 1)
+
+
+def get_solutions():
+    '''Gets the solutions for the given chessboard size.
+    '''
+    global pos, n
+    pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
+    a = 0
+    group = []
+    build_solution(a, group)
+
+
+n = get_input()
+get_solutions()
+for solution in result:
+    print(solution)
