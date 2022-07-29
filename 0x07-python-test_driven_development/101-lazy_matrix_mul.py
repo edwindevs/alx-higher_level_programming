0ecf890a47c80ff70fafbb515ff9c2eb273993d7
edwindevs <edmbogo@outlook.com>
2022-07-29 23:21:50 +0000

    Test driven development

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-'''a lazy_matrix_mul function'''
+"""A module that contains the function 'lazy_matrix_mul'"""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    '''Multiplies 2 matrices.'''
-    a = np.array(m_a)
-    b = np.array(m_b)
-    return np.matmul(a, b)
+    """returns the product of two conformable matrices in a lazy way"""
+    return np.matmul(m_a, m_b)
