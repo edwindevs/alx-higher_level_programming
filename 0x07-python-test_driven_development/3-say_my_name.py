fbe9b80c1c15b330b1d1ec87a9d68664f2888f7e
edwindevs <edmbogo@outlook.com>
2022-07-29 23:24:04 +0000

    Test driven development

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+"""A module that contains a function that prints the full name of a person"""
+
+
 def say_my_name(first_name, last_name=""):
-    """function that prints My name is <first name> <last name>"""
-
-    if not first_name or type(first_name) != str:
+    """Says the full name of a person
+    Args:
+        first_name (str): the first name provided
+        last_name (str): the last name provided - optional
+    Format of output:
+        My name is <first_name> <last_name>
+    Exceptions:
+        TypeError - If first_name or last_name is not a string
+    """
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    elif type(last_name) != str:
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-
-    print("My name is {} {}".format(first_name, last_name))
+    print(f"My name is {first_name} {last_name}")
